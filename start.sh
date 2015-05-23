2a5975da9c42c69a7fd11a4427035a3ff3008902
Patrick Schwartz <kurogit@schwartzm.com>
2015-05-23 19:55:10 +0200

    Add config change for remote connections

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ set -e
 
 rm -f /data/deluged.pid
 
-deluged -c /data -L info -l /data/deluged.log && tail -F /data/deluged.log
+deluged -c /data -L info -l /data/deluged.log
+deluge-console -c /data "config -s allow_remote True"
+tail -F /data/deluged.log
